1c83dd8e32ea6c367635d72c922e4c90ae8d3ab7
Aviad Moreshet <aviad10m@gmail.com>
2018-11-24 17:20:21 +0200

    Renamed dotenv file to .env

--- helm-dep-up-umbrella.sh
+++ helm-dep-up-umbrella.sh
@@ -6,10 +6,12 @@ if [ -e requirements.yaml ]; then
   for subDir in $(awk -F'repository: file://' '{print $2}' requirements.yaml)
   do
     echo "Updating dependencies in "$(pwd)"/"$subDir" ..."
+    rm -r "$(pwd)"/"$subDir"/charts
     helm dep up "$(pwd)"/"$subDir"
     echo
   done
   echo "Updating dependencies in "$(pwd)" ..."
+  rm -r "$(pwd)"/charts
   helm dep up "$(pwd)"
   echo
 fi
