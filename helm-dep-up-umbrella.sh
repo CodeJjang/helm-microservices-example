87375daf9a983627cbd46e69dc9b6b55a9737a30
Aviad Moreshet <aviad10m@gmail.com>
2018-12-01 20:08:16 +0200

    Volume mounts work

--- helm-dep-up-umbrella.sh
+++ helm-dep-up-umbrella.sh
@@ -6,12 +6,12 @@ if [ -e requirements.yaml ]; then
   for subDir in $(awk -F'repository: file://' '{print $2}' requirements.yaml)
   do
     echo "Updating dependencies in "$(pwd)"/"$subDir" ..."
-    rm -r "$(pwd)"/"$subDir"/charts
+    rm -rf "$(pwd)"/"$subDir"/charts
     helm dep up "$(pwd)"/"$subDir"
     echo
   done
   echo "Updating dependencies in "$(pwd)" ..."
-  rm -r "$(pwd)"/charts
+  rm -rf "$(pwd)"/charts
   helm dep up "$(pwd)"
   echo
 fi
